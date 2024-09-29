7d01db087d7df774fbcc0560fc8ba8b8d5479d6f
Jazz <167805092+JazzCourter@users.noreply.github.com>
2024-09-28 23:46:24 -0400

    Add files via upload

---
+++ Raining Python-JazzCourter.py
@@ -0,0 +1,15 @@
+# Define variabales
+#==================
+
+# Variable c: noun 1
+c = "cats"
+
+#Variable d: noun 2
+d = "dogs"
+
+#Variable s: sentence to print
+s = f"It is raining {c} and {d}."
+
+# Print the sentence
+# ==================
+print(s)
