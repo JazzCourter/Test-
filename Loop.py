29bda93bcd8ad7835418622b0419616438d33695
Jazz <167805092+JazzCourter@users.noreply.github.com>
2024-10-06 03:17:25 -0400

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,12 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+# Loop through each number in the list
+for number in numbers:
+    # Check if the number is even
+    if number % 2 == 0:
+        # Print the number is even
+        print(str(number) + " is even")
+    else:
+        # Print the number is odd
+        print(str(number) + " is odd")
